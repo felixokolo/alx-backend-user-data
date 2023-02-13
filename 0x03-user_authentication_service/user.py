b8cc52043e595bd7e6dca7cb6545a1605f02d7c9
felixokolo <pheelix.info@gmail.com>
2023-02-13 22:33:28 +0100

    First commit

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""User authentication"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User table model"""
+
+    __tablename__: str = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
