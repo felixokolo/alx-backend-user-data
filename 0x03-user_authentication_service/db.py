86bd320db13cb9f4af818a06af208448ee1f771b
felixokolo <pheelix.info@gmail.com>
2023-02-14 14:44:47 +0100

    additions

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Saves user to database"""
+        new_user = User()
+        new_user.email = email
+        new_user.hashed_password = hashed_password
+        return new_user
+
+    def find_user_by(self, **kwargs):
+        """finds a user based on input keywords"""
+        self.__session.query(User).filter_by(**kwargs)
